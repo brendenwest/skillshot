a6ebf38bce8d803772eee972b23d1ea7cde70a73
Brenden West <brendenw@hotmail.com>
2017-10-25 23:31:43 -0700

    add default map view w marker

--- app/src/main/java/android/skillshot/com/skillshot/MainActivity.java
+++ app/src/main/java/android/skillshot/com/skillshot/MainActivity.java
@@ -1,15 +1,29 @@
 package android.skillshot.com.skillshot;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.gms.maps.CameraUpdate;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity implements OnMapReadyCallback {
+
+    private final int SKILL_SHOT_YELLOW = 42;
+    private static final float DEFAULT_ZOOM = 15;
+    public static double SHORTYS_LAT = 47.613834;
+    public static double SHORTYS_LONG = -122.345043;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,6 +32,10 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        SupportMapFragment mapFragment =
+                (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+
     }
 
     @Override
@@ -25,6 +43,29 @@ public class MainActivity extends AppCompatActivity {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
+    }
+
+    /**
+     * This is where we can add markers or lines, add listeners or move the camera. In this case,
+     * we just add a marker centered on downtown Seattle.
+     */
+    @Override
+    public void onMapReady(GoogleMap map) {
+        CameraUpdate center=
+                CameraUpdateFactory.newLatLng(new LatLng(SHORTYS_LAT,
+                        SHORTYS_LONG));
+        CameraUpdate zoom=CameraUpdateFactory.zoomTo(DEFAULT_ZOOM);
+
+        map.moveCamera(center);
+        map.animateCamera(zoom);
+
+        // marker demo to replace w/ collection of markers from API data
+        map.addMarker(new MarkerOptions()
+                .position(new LatLng(47.6120477,
+                        -122.3396271))
+                .title("Downtown")
+                .icon(BitmapDescriptorFactory.defaultMarker(SKILL_SHOT_YELLOW))
+                .snippet("Skill Shot HQ"));
     }
 
     @Override
